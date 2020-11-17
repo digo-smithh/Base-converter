1561f2507e13063b24dfade5a42060c41afeb1e6
digo-smithh <smithrodrigues08@gmail.com>
2020-11-17 19:32:16 -0300

    Adjustments

--- Base converter/main.c
+++ Base converter/main.c
@@ -291,7 +291,7 @@ int main()
     doAsciiArt();
     number = (char *) malloc(1000 * sizeof(char));
 
-    printf("\n        Developed by: Eduardo Migueis - 19167 and Rodrigo Smith - 19197\n");
+    printf("\n        Developed by: Eduardo Migueis and Rodrigo Smith\n");
     fflush(stdout);
 
     printf("\n  Enter the number to be converted: ");
